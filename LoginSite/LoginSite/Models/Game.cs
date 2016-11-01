0b1f74b072d346b73f47978c28c760ad3422e9e9
j0hnnatan <j_segura_araya@hotmail.com>
2016-10-31 22:00:15 -0600

    Proyecto-PartidosFut5
    Nuevo proyecto con la coneccion a la base de datos,  validacion de
    usuarios existentes y registro de usuarios nuevos, hashing de la
    contraseña, login 100% funcional

---
+++ LoginSite/LoginSite/Models/Game.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace LoginSite.Models
+{
+    public class Game                       //cualqueir partido tiene 4 valores, ID:valor unico, detalles, fecha y hora
+    {
+        public ObjectId Id { get; set; }
+        public string detail { get; set; }
+        public string date { get; set; }
+        public string time { get; set; }
+    }
+}
